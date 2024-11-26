9cdac15644208c8f075f2f7418b1fab4435dc39e
CarlaSerraoOn <carla.suporteam@gmail.com>
2024-11-25 21:02:05 -0400

    commitando código

---
+++ cypress/e2e/cadastro_usuario.cy.js
@@ -0,0 +1,31 @@
+describe('Cadastro de usuário', () => {
+
+    beforeEach('Acessar cadastro de usuário', () => {
+        
+    })
+
+    it('Campo Nome vazio', () => {
+       
+    })
+
+    it('Campo E-mail vazio', () => {
+      
+    })
+
+    it('Campo E-mail inválido', () => {
+    
+    })
+
+    it('Campo Senha vazio', () => {
+       
+    })
+
+    it('Campo Senha inválido', () => {
+     
+    })
+
+    it('Cadastro feito com sucesso', () => {
+     
+    })
+    
+})
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,27 @@
+describe('Login', () => {
+
+    it('Login com sucesso', () => {
+        // Os comandos de testes
+    })
+
+    it('Campo Nome vazio', () => {
+        // Os comandos de testes
+    })
+
+    it('Campo E-mail vazio', () => {
+        // Os comandos de testes
+    })
+
+    it('Campo E-mail inválido', () => {
+        // Os comandos de testes
+    })
+
+    it('Campo Senha vazio', () => {
+        // Os comandos de testes
+    })
+
+    it('Campo Senha inválido', () => {
+        // Os comandos de testes
+    })
+
+})
