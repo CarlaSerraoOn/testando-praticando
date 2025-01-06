c83bf6880f662df7490fdfd37f1e8d4358e8249a
CarlaSerraoOn <carla.suporteam@gmail.com>
2025-01-06 11:33:34 -0300

    cypress.io

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'mix4kx',
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
